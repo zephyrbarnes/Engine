01d35481fa63c1fa19aec700e09102f628c727c4
Zephyr Barnes <zepbar429@gmail.com>
2023-08-24 23:41:24 -0600

    hfds

--- engine.js
+++ engine.js
@@ -1,99 +1,103 @@
 let cnv, ctx, cmr, myCube, d = 10;
-const pie = Math.PI,
-      tau = 2 * pie,
-      deg = tau / 360;
+var cos = Math.cos, sin = Math.sin, tan = Math.tan
+const pie = Math.PI, tau = 2 * pie, deg = tau / 360;
 
-class view {
-    constructor(xAxi,yAxi,zAxi,bite){
-        this.matrix = [
-            [ xAxi.x, yAxi.x, zAxi.x, bite.x],
-            [ xAxi.y, yAxi.y, zAxi.y, bite.y],
-            [ xAxi.z, yAxi.z, zAxi.z, bite.z],
-            [      0,      0,      0,      1]];
-        this.translate = [
-            [      1,      0,      0,-bite.x],
-            [      0,      1,      0,-bite.y],
-            [      0,      0,      1,-bite.z],
-            [      0,      0,      0,      1]];
-    }
+class view { constructor(fov,far,near) { this.bite;
+    let s = 1/tan(fov*deg/2), c = far-near, c1 = -(far+near)/c, c2 = -(2*far*near)/c; this.proj = [
+        [ s, 0, 0, 0],
+        [ 0, s, 0, 0],
+        [ 0, 0,c1,-1],
+        [ 0, 0,c2, 0]];}
+    port() { return this.bite.map(value => -value); }}
+
+class bite extends Array{ constructor(x,y,z) { let w = 1; super(x,y,z,w); }
+    mult(m) { for (var i = 0; i < 4; i++) { this[i] = Number(Math.round(x*m[i][0] + y*m[i][1] + z*m[i][2] + w*m[i][3]+'e2')+'e-2'); }}
+    move(b) { let m = [[b[0],0,0,0],[0,b[1],0,0],[0,0,b[2],0],[0,0,0,0]]; mult(m);}}
+class trig extends Array{ constructor(bit1,bit2,bit3) { super(bit1,bit2,bit3); this.color; }
+    mult(m) { for (var i = 0; i < 3; i++) { this[i].mult(m); }}
+    move(b) { for (var i = 0; i < 3; i++) { this[i].move(b); }}}
+
+class face extends Array{ constructor(bit1,bit2,bit3,bit4) { super(bit1,bit2,bit3,bit4); this.color; }
+    mult(m) { for (var i = 0; i < 4; i++) { this[i].mult(m); }}
+    move(b) { for (var i = 0; i < 4; i++) { this[i].move(b); }}}
+
+class cube { constructor(bite,color,size) { this.cord = bite; this.color = color; let s = size/2, x = bite[0], y = bite[1], z = bite[2];
+    this.bites = [ new bite(x+s, y+s, z-s),new bite(x+s, y-s, z-s),
+                   new bite(x+s, y-s, z+s),new bite(x+s, y+s, z+s),
+                   new bite(x-s, y+s, z+s),new bite(x-s, y-s, z+s),
+                   new bite(x-s, y-s, z-s),new bite(x-s, y+s, z-s)];
+    this.faces = [ new face(bites[1],bites[0],bites[3],bites[2]),
+                   new face(bites[2],bites[3],bites[4],bites[5]),
+                   new face(bites[3],bites[0],bites[7],bites[4]),
+                   new face(bites[4],bites[5],bites[6],bites[7]),
+                   new face(bites[5],bites[6],bites[1],bites[2]),
+                   new face(bites[6],bites[7],bites[0],bites[1])];
+        for(let face of this.faces) { face.move(this.cord) }}
+    mult(m) { for(let face of this.faces) { face.mult(m); }}
+    move(b) { for(let face of this.faces) { face.move(b); }}
 }
 
-function projection(fov,far,near){
-    let s = 1 / Math.tan(fov*deg/2)
-    let c1 = -(far + near) / (far - near), c2 = -(2*far * near) / (far - near);
-    this.proj = [[s, 0, 0, 0],[0, s, 0, 0],[0, 0,c1,c2],[0, 0,-1, 0]];
-}
-
-class bite extends Array{ constructor(x,y,z,w) { super(x,y,z,w);}}
-class trig extends Array{ constructor(bit1,bit2,bit3,color) { super(bit1,bit2,bit3,color); }}
-class face extends Array{ constructor(bit1,bit2,bit3,bit4,color) { super(bit1,bit2,bit3,bit4,color);}}
-class cube { constructor(x,y,z,size,color) { this.size = size/2; this.color = color; this.x = x; this.y = y; this.z = z;
-        this.bites = [
-            new bite(x+s, y+s, z-s,1),/*++-*/
-            new bite(x+s, y-s, z-s,1),/*+--*/
-            new bite(x+s, y-s, z+s,1),/*+-+*/
-            new bite(x+s, y+s, z+s,1),/*+++*/
-            new bite(x-s, y+s, z+s,1),/*-++*/
-            new bite(x-s, y-s, z+s,1),/*--+*/
-            new bite(x-s, y-s, z-s,1),/*---*/
-            new bite(x-s, y+s, z-s,1),/*-+-*/
-        ];
-        this.faces = [
-            new face(this.bites[2], this.bites[3], this.bites[4], this.bites[5], this.color),
-            new face(this.bites[5], this.bites[6], this.bites[1], this.bites[2], this.color),
-            new face(this.bites[2], this.bites[1], this.bites[0], this.bites[3], this.color),
-            new face(this.bites[3], this.bites[0], this.bites[7], this.bites[4], this.color),
-            new face(this.bites[4], this.bites[5], this.bites[6], this.bites[7], this.color),
-            new face(this.bites[7], this.bites[0], this.bites[1], this.bites[6], this.color)];
-    }
-}
-
-class v2 { constructor(x, y) { this.x = parseFloat(x); this.y = parseFloat(y); } }
-
-var cos = Math.cos, sin = Math.sin;
-
-function rote(matrix,axis,tang) { let result, a = tang*deg;
-    if(axis == 'x') { result = [
+function rote(axis,tang) { let rslt, a = tang*deg;
+    if(axis == 'x') { rslt = [
         [ 1,    0  ,    0  , 0],
         [ 0, cos(a),-sin(a), 0],
         [ 0, sin(a), cos(a), 0],
         [ 0,    0  ,    0  , 1]];
-    }else if(axis == 'y') { result = [
+    }else if(axis == 'y') { rslt = [
         [ cos(a), 0, sin(a), 0],
         [    0  , 1,    0  , 0],
         [-sin(a), 0, cos(a), 0],
         [    0  , 0,    0  , 1]];
-    }else{ result = [
+    }else{ rslt = [
         [ cos(a),-sin(a), 0, 0],
         [ sin(a), cos(a), 0, 0],
         [    0  ,    0  , 0, 0],
         [    0  ,    0  , 0, 1]];
-    } return mult(result,mtrx);
+    } return rslt;
 }
 
-function mult(a,b) { var result = [[],[],[],[]];
+function mult(a,b) { var rslt = [[],[],[],[]];
     for(var i = 0;i < aR; i++) { 
         for (var j = 0; j < 4; j++) {
-            for (var k = 0; k < 4; k++) { result[i][j] = a[i][k]*b[k][j]; }      
+            for (var k = 0; k < 4; k++) { rslt[i][j] = a[i][k]*b[k][j]; }
         }  
-    } return result;
+    } return rslt;
+}
+
+function rend(objs) {
+    let fake = objs;
+    fake.move(cmr.port());
+	for (var i = 0, n_obj = objs.length; i < n_obj; ++i) {
+		for (var j = 0, n_faces = objs[i].faces.length; j < n_faces; ++j) { var face = objs[i].faces[j];
+			ctx.beginPath();
+			ctx.moveTo(P.x + dx, -P.y + dy);
+
+			for (var k = 1, n_bites = face.length; k < n_bites; ++k) {
+				
+				ctx.lineTo(P.x + dx, -P.y + dy);
+			}
+
+			// Close the path and draw the face
+			ctx.closePath();
+			ctx.stroke();
+			ctx.fill();
+		}
+	}
 }
 
 function start() {
     cnv = document.getElementById('cnv'); ctx = cnv.getContext('2d');
     cnv.width = window.innerWidth; cnv.height = window.innerHeight;
-    cmr = new view(new bite(1,0,0),new bite(0,1,0),new bite(0,0,1),new bite(0,0,-10));
-    cmr.projection(75,100,0.1);
-    ctx.strokeStyle = 'rgba(0, 0, 0, 0.3)';
+    cmr = new view(75,100,0.1); cmr.bite = new bite(0,0,-10);
     ctx.fillStyle = 'rgba(0, 150, 255, 0.3)';
-    myCube = new cube(100, new bite(0, 0, 0), 'rgba(0, 150, 255, 1)');
+    ctx.strokeStyle = 'rgba(0, 0, 0, 0.3)';
+    myCube = new cube(100, new bite(0, 0, 0),'rgba(0, 150, 255, 1)');
+    var objs = []; objs.push(myCube);
     run();
 }
 
 function run() {
     ctx.clearRect(0, 0, cnv.width, cnv.height);
-    
-    rend(myCube)
 
     setTimeout(requestAnimationFrame(run),30);
 }
