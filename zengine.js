5cf84563a1f2ffa91de052ecceb83d090d47e433
Zephyr Barnes <zepbar429@gmail.com>
2023-08-30 00:01:25 -0600

    hmm camera...

--- zengine.js
+++ zengine.js
@@ -23,6 +23,8 @@ class Mesh { constructor(path, C=new V,S=0, R=new V) { this.S = S; this.R = R; t
     }});}
 }
 
+class Camera { constructor(C=new V,R = new V) { this.C = C; this.R = R; }}
+
 async function loadTextFile(fl) {
     try { const rt = await fetch(fl); if(!rt.ok) { throw new Error(); } const txt = await rt.text(); return txt;}
     catch(er) { console.error(er); return null; }
@@ -75,8 +77,8 @@ function rotate(v,r) { const [x,y,z] = [r.x*dg, r.y*dg, r.z*dg], [cx,cy,cz,sx,sy
     const [vysx, vycx, vzcx, czsy, szsy] = [(v.y*sx), (v.y*cx), (v.z*cx), (cz*sy), (sz*sy)]; /*Filler const for reduction*/
     const rX = v.x*(cz*cy) + (vysx*czsy) - (vycx*sz) + (vzcx*czsy) + v.z*(sz*sx); /*Rotation of the X axi*/
     const rY = v.x*(sz*cy) + (vysx*szsy) + (vycx*cz) + (vzcx*szsy) - v.z*(cz*sx); /*Rotation of the Y axi*/
-    /*Demo const*/const rZ = (vysx * cy) + (-v.x*sy) + (vzcx * cy); /*Rotation of the Z axi*/
-    return new V(rX, rY, rZ); }
+    /*Demo const*/const rZ = (vysx * cy) + (-v.x*sy) + (vzcx * cy); /*Rotation of the Z axi*/ const rW = v.w;
+    return new V(pF(rX), pF(rY), pF(rZ), pF(rW)); }
 
 function translate(v, c) { const x = pF(v.x + c.x), y = pF(v.y + c.y), z = pF(v.z + c.z), w = pF(v.w + c.w); return new V(x,y,z,w); }
 function subtract(v, c) { const x = pF(v.x - c.x), y = pF(v.y - c.y), z = pF(v.z - c.z), w = pF(v.w - c.w); return new V(x,y,z,w); }
@@ -95,14 +97,14 @@ function drawPoly(v,face) {
     let cm = subtract(f[0], cam); let nl = normalValue(normalOfFace(f)), nf = dotProduct(nl,cm);
     if (nf < 0) {
         let gi = dotProduct(nl, lit); f.rgba = `rgba(0, ${parseFloat((gi*150).toFixed(0))}, ${parseFloat((gi*255).toFixed(0))}, 1)`;
-        var depth = 0; for(let v of f) { depth+=abs(v.z); } depth/=f.length; f.depth = parseFloat(depth.toFixed(4));
+        //var depth = 0; for(let v of f) { depth+=abs(v.z); } depth/=f.length; f.depth = parseFloat(depth.toFixed(4)); 
         for(let i=0; i < f.length; i++) { f[i] = projects(f[i]); }
         toDraw.push(f);
     }
 }
 
-function drawObjs(item) { var vs = transforms(item);
-    for (let i = 0; i < item.F.length; i++) { drawPoly(vs, item.F[i]); }
+function drawObjs(item) { var vs = transforms(item); 
+    for (let i = 0; i < item.F.length; i++) { drawPoly(vs, item.F[i]);}
     toDraw.sort((a, b) => b.depth - a.depth);
     for(let face of toDraw) { vectLine(face);
         ctx.strokeStyle = face.rgba;
@@ -113,8 +115,8 @@ function drawObjs(item) { var vs = transforms(item);
 
 function transforms(obj) {
     const b = []; for (let v of obj.B) { b.push({x:v.x,y:v.y,z:v.z,w:v.w}); }
-    var dir = new V(0,0,1), abv = new V(0,1,0), trg = translate(cam, dir), matCam = pointAt(cam, trg, abv), matView = quickInverse(matCam);
-    for(let i=0; i < b.length; i++) { b[i] = translate(rotate(scale(b[i],obj.S),obj.R),obj.C); }
+    //var dir = new V(0,0,1), abv = new V(0,1,0), trg = translate(cam, dir), matCam = pointAt(cam, trg, abv), matView = quickInverse(matCam);
+    for(let i=0; i < b.length; i++) { b[i] = rotate(translate(rotate(scale(b[i],obj.S),obj.R),obj.C),cam.R); }
     return b;
 }
 
@@ -124,8 +126,8 @@ const cv = document.getElementById('cv'), ctx = cv.getContext('2d'), cvw = cv.wi
 const c=Math.cos, s=Math.sin, tan=Math.tan, abs=Math.abs, rt=Math.sqrt, pi=Math.PI, dg=pi/180, fs = false, tr = true;
 var nie=0.01, far=22, fov=70, dbug = fs; calcView();
 var objs = [], toDraw = [];
-var cam = new V();
-var origin = new Mesh("origin_axis.obj");
+var cam = new Camera();
+var origin = new Mesh("origin_axis.obj", new V(0,0,+50));
 var body = new Mesh("reduced.obj", new V(0,0,6));
 var lit = new V(1,1,-1); normal(lit);
 
