8fecd4b0167f138443feb3ca68a167c7590c1e39
Zephyr Barnes <zepbar429@gmail.com>
2023-09-04 20:59:19 -0600

    Totally rewrote it, so much better, got working

--- input.js
+++ input.js
@@ -1,39 +1,45 @@
-const fs = false, tr = true;
-class keyHandler {
+const f = false, t = true, abs = Math.abs;
+class controller {
   keyState = {
-                      87: fs, /*W KEY*/
-    65: fs, /*A KEY*/ 83: fs, /*S KEY*/ 68: fs, /*D KEY*/
-    32: fs, /*SPACE*/					38: fs, /*ABOVE*/
-    37: fs, /*LEFTS*/ 40: fs, /*BELOW*/ 39: fs, /*RIGHT*/
-    13: fs  /*ENTER*/
+    27: f, /*EXITS*/ 87: f, /*W KEY*/
+    65: f, /*A KEY*/ 83: f, /*S KEY*/ 68: f, /*D KEY*/
+    32: f, /*SPACE*/ 38: f, /*ABOVE*/
+    37: f, /*LEFTS*/ 40: f, /*BELOW*/ 39: f, /*RIGHT*/
+    13: f  /*ENTER*/
   };
+
+  constructor() {
+    document.addEventListener('keydown', (e) => this.pass(e, true));
+    document.addEventListener('keyup', (e) => this.pass(e, false));
+    document.addEventListener("mousemove", (e) => this.cont(e));
+    document.addEventListener("mousedown", function(e) { cv.requestPointerLock(); });
+  }
   pass(e, isPressed) { if (e.which in this.keyState) { this.keyState[e.which] = isPressed; e.preventDefault(); } }
+  cont(e) {
+    var roteSp = 0.1;
+    var deltaX = e.movementY;
+    var deltaY = e.movementX;
+  
+    cam.R.x -= deltaX * roteSp;
+    cam.R.y += deltaY * roteSp;
+  }
   key(keyCode) { return this.keyState[keyCode]; }
 }
 
-document.addEventListener('keydown', (e) => keyH.pass(e, true));
-document.addEventListener('keyup', (e) => keyH.pass(e, false));
-
-var keyH = new keyHandler(), key = keyH.keyState;
-cv.addEventListener("mousemove", function(e) {
-  var deltaX = e.clientX - prevMouseX;
-  var deltaY = e.clientY - prevMouseY;
-  //camera.R.y += deltaX * rotateSpeed;
-  //camera.R.x += deltaY * rotateSpeed;
-
-  //camera.R.x = Math.max(Math.min(camera.R.x, 90), -90);
-});
-var moveSpeed = 2, rotateSpeed = 0.5;
-var prevMouseX = 0, prevMouseY = 0;
-
+var keyH = new controller(), key = keyH.keyState;
 
 function keysCheck() {
-  if (!key[87] && key[83] || key[87] && !key[83]) {a
-    if (key[87]) camera.T = addVector(camera.T, camera.S);
-    if (key[83]) camera.T = subVector(camera.T, camera.S);
-  }
-  if (!key[65] && key[68] || key[65] && !key[68]) {
-    if (key[65]) camera.R.y -= 0.001;
-    if (key[68]) camera.R.y += 0.001;
-  }
+    if (!key[87] && key[83] || key[87] && !key[83] || !key[38] && key[40] || key[38] && !key[40]) {
+        camF = mulVector(cam.D,cam.S);
+        if (key[87] || key[38]) cam.P = vecSubVec(cam.P,camF);
+        if (key[83] || key[40]) cam.P = vecAddVec(cam.P,camF);
+    }
+    if (!key[65] && key[68] || key[65] && !key[68] || !key[37] && key[39] || key[37] && !key[39]) {
+        let camX = crossProd(cam.D,cam.U);
+            camX = divVector(camX,sqrt(inProduct(camX,camX)));
+            camX = mulVector(camX,cam.S);
+        if (key[65] || key[37]) cam.P = vecAddVec(cam.P,camX);
+        if (key[68] || key[39]) cam.P = vecSubVec(cam.P,camX);
+    }
+    if (key[27]) document.exitPointerLock();
 }
